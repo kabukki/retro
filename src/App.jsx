88d55655ee7f57497a7e6ec63d8c687c76fd9a14
Lucien Le Roux <rshn.kabu@gmail.com>
2021-11-18 18:43:30 +0100

    Updated home screen

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,24 @@ import { Helmet } from 'react-helmet';
 import { Chip8 } from './chip8';
 import { Nes } from './nes';
 
+import odyssey from './assets/odyssey.png';
 import chip8 from './assets/chip8.png';
 import nes from './assets/nes.png';
+import megadrive from './assets/megadrive.png';
+import atari2600 from './assets/atari2600.png';
+import sms from './assets/sms.png';
 import gameboy from './assets/gameboy.png';
+import snes from './assets/snes.png';
 import n64 from './assets/n64.png';
+import neogeo from './assets/neogeo.png';
 import playstation from './assets/playstation.png';
+import playstation2 from './assets/playstation2.png';
+import saturn from './assets/saturn.png';
+import gamecube from './assets/gamecube.png';
+import xbox from './assets/xbox.png';
+
 import Github from './assets/github.svg';
+import Wikipedia from './assets/wikipedia.svg';
 
 const withTitle = (emulator) => (props) => {
     const { name, component: Component } = emulator;
@@ -28,58 +40,179 @@ const withTitle = (emulator) => (props) => {
 const emulators = [
     {
         title: 'CHIP-8',
-        subtitle: 'Weisbecker, 1978',
-        path: '/chip8',
+        developer: 'Weisbecker',
+        year: 1978,
+        generation: null,
+        wikipedia: 'https://en.wikipedia.org/wiki/CHIP-8',
+        github: 'https://github.com/kabukki/wasm-chip8',
+        path: '/other/chip8',
         picture: chip8,
-        url: 'https://github.com/kabukki/wasm-chip8',
         component: Chip8,
     }, {
+        title: 'Odyssey',
+        developer: 'Magnavox',
+        year: 1972,
+        generation: 1,
+        wikipedia: 'https://fr.wikipedia.org/wiki/Magnavox_Odyssey',
+        path: '/magnavox/odyssey',
+        picture: odyssey,
+        component: () => <p>Coming soon(ish)</p>,
+    }, {
+        title: 'Atari 2600',
+        developer: 'Atari',
+        year: 1977,
+        generation: 2,
+        wikipedia: 'https://en.wikipedia.org/wiki/Atari_2600',
+        path: '/atari/2600',
+        picture: atari2600,
+        component: () => <p>Coming soon(ish)</p>,
+    }, {
         title: 'NES',
-        subtitle: 'Nintendo, 1983',
-        path: '/nes',
+        developer: 'Nintendo',
+        year: 1983,
+        generation: 3,
+        wikipedia: 'https://en.wikipedia.org/wiki/Super_Nintendo_Entertainment_System',
+        github: 'https://github.com/kabukki/wasm-nes',
+        path: '/nintendo/nes',
         picture: nes,
-        url: 'https://github.com/kabukki/wasm-nes',
         component: Nes,
     }, {
+        title: 'Master System',
+        developer: 'Sega',
+        year: 1986,
+        generation: 3,
+        wikipedia: 'https://en.wikipedia.org/wiki/Master_System',
+        path: '/sega/master-system',
+        picture: sms,
+        component: () => <p>Coming soon(ish)</p>,
+    }, {
+        title: 'Mega Drive',
+        developer: 'Sega',
+        year: 1988,
+        generation: 4,
+        wikipedia: 'https://en.wikipedia.org/wiki/Sega_Genesis',
+        path: '/sega/megadrive',
+        picture: megadrive,
+        component: () => <p>Coming soon(ish)</p>,
+    }, {
         title: 'Game Boy',
-        subtitle: 'Nintendo, 1989',
-        path: '/gameboy',
+        developer: 'Nintendo',
+        year: 1989,
+        generation: 4,
+        wikipedia: 'https://en.wikipedia.org/wiki/Game_Boy',
+        path: '/nintendo/gameboy',
         picture: gameboy,
         component: () => <p>Coming soon(ish)</p>,
     }, {
+        title: 'Super NES',
+        developer: 'Nintendo',
+        year: 1990,
+        generation: 4,
+        wikipedia: 'https://en.wikipedia.org/wiki/Super_Nintendo_Entertainment_System',
+        path: '/nintendo/snes',
+        picture: snes,
+        component: () => <p>Coming soon(ish)</p>,
+    }, {
+        title: 'Neo Geo',
+        developer: 'SNK',
+        year: 1990,
+        generation: 4,
+        wikipedia: 'https://fr.wikipedia.org/wiki/Neo-Geo_AES',
+        path: '/snk/neogeo',
+        picture: neogeo,
+        component: () => <p>Coming soon(ish)</p>,
+    }, {
         title: 'PlayStation',
-        subtitle: 'Sony, 1994',
-        path: '/ps',
+        developer: 'Sony',
+        year: 1994,
+        generation: 5,
+        wikipedia: 'https://en.wikipedia.org/wiki/PlayStation_(console)',
+        path: '/sony/playstation',
         picture: playstation,
         component: () => <p>Coming soon(ish)</p>,
     }, {
+        title: 'Saturn',
+        developer: 'Sega',
+        year: 1994,
+        generation: 5,
+        wikipedia: 'https://en.wikipedia.org/wiki/Sega_Saturn',
+        path: '/sega/saturn',
+        picture: saturn,
+        component: () => <p>Coming soon(ish)</p>,
+    }, {
         title: 'Nintendo 64',
-        subtitle: 'Nintendo, 1996',
-        path: '/n64',
+        developer: 'Nintendo',
+        year: 1996,
+        generation: 5,
+        wikipedia: 'https://en.wikipedia.org/wiki/Nintendo_64',
+        path: '/nintendo/64',
         picture: n64,
         component: () => <p>Coming soon(ish)</p>,
-    }
+    }, {
+        title: 'PlayStation 2',
+        developer: 'Sony',
+        year: 2000,
+        generation: 6,
+        wikipedia: 'https://en.wikipedia.org/wiki/PlayStation_2',
+        path: '/sony/playstation2',
+        picture: playstation2,
+        component: () => <p>Coming soon(ish)</p>,
+    }, {
+        title: 'GameCube',
+        developer: 'Nintendo',
+        year: 2001,
+        generation: 6,
+        wikipedia: 'https://en.wikipedia.org/wiki/GameCube',
+        path: '/nintendo/gamecube',
+        picture: gamecube,
+        component: () => <p>Coming soon(ish)</p>,
+    }, {
+        title: 'Xbox',
+        developer: 'Microsoft',
+        year: 2001,
+        generation: 6,
+        wikipedia: 'https://en.wikipedia.org/wiki/Xbox_(console)',
+        path: '/microsoft/xbox',
+        picture: xbox,
+        component: () => <p>Coming soon(ish)</p>,
+    },
 ];
 
-const Card = ({ to, picture, title, subtitle, url }) => (
-    <div className="relative flex items-center p-2 gap-2 rounded shadow overflow-hidden group hover:bg-green-700 hover:text-white transition">
-        {!url && (
-            <div className="absolute inset-0 flex flex-col justify-center bg-gray-900 bg-opacity-50">
-                <p className="py-4 text-center text-white">Unavailable</p>
+const Card = ({ title, developer, year, generation, path, picture, github, wikipedia }) => (
+    <div className={`relative overflow-hidden rounded shadow divide-y bg-${github ? 'white' : 'gray-100 opacity-25'} transition hover:shadow-md`}>
+        <h1 className="flex">
+            <div className={`p-2 w-10 h-10 flex items-center justify-center bg-green-${200 + generation * 100} text-white`}>
+                {generation || '-'}
             </div>
-        )}
-        <img className="h-24 object-contain" src={picture} alt={title} />
-        <div className="flex-1">
-            <Link to={to}>
-                <h2 className="font-bold text-green-700 transition group-hover:text-white">{title}</h2>
-                {subtitle}
-            </Link>
+            <div className="p-2 flex-1 flex gap-2">
+                {github ? (
+                    <Link to={path} className="flex-1 text-green-700 font-bold font-mono">
+                        {title}
+                    </Link>
+                ) : (
+                    <span className="flex-1 line-through">
+                        {title}
+                    </span>
+                )}
+                {github && (
+                    <a href={github} target={`${title}:github`} className="transition hover:text-green-700">
+                        <Github className="fill-current"/>
+                    </a>
+                )}
+                {wikipedia && (
+                    <a href={wikipedia} target={`${title}:wikipedia`} className="transition hover:text-green-700">
+                        <Wikipedia className="fill-current"/>
+                    </a>
+                )}
+            </div>
+        </h1>
+        <div className="p-2 flex items-center gap-2">
+            <img className="h-24 object-contain" src={picture} alt={title} />
+            <div className="flex-1">
+                <h2 className="font-bold">{developer}</h2>
+                {year}
+            </div>
         </div>
-        {url && (
-            <a href={url} target={title} >
-                <Github className="fill-current"/>
-            </a>
-        )}
     </div>
 );
 
@@ -92,7 +225,7 @@ export const App = () => {
                         <Link to="/">RETRO</Link>
                     </h1>
                     <ul className="flex flex-wrap items-center gap-4">
-                        {emulators.map(({ title, picture, path }) => (
+                        {emulators.filter(({ github }) => !!github).map(({ title, picture, path }) => (
                             <li key={title}>
                                 <NavLink className="flex items-center gap-2" to={path} activeClassName="font-bold">
                                     <img className="h-4" src={picture} />
@@ -106,13 +239,15 @@ export const App = () => {
                     {emulators.map((emulator) => (
                         <Route key={emulator.title} path={emulator.path} component={withTitle(emulator)} />
                     ))}
-                    <Route path="/" exact>
-                        <h1 className="m-4 text-xl text-center">Welcome to RETRO !</h1>
-                        <div className="m-4 grid gap-4 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-                            {emulators.map(({ title, subtitle, path, picture, url }) => (
-                                <Card key={title} to={path} picture={picture} title={title} subtitle={subtitle} url={url} />
-                            ))}
-                        </div>
+                    <Route path="/">
+                        <main className="m-4 flex flex-col gap-4">
+                            <h1 className="text-xl text-center">Welcome to RETRO !</h1>
+                            <div className="grid gap-4 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+                                {emulators.map((emulator) => (
+                                    <Card key={emulator.title} {...emulator} />
+                                ))}
+                            </div>
+                        </main>
                     </Route>
                 </Switch>
             </Router>
