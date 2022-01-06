80c9677f2841c15563f95d4dfa5eb159c78a9336
Lucien Le Roux <rshn.kabu@gmail.com>
2022-01-06 16:28:52 +0100

    added missing jsx imports

--- src/emulators/nintendo/64/index.jsx
+++ src/emulators/nintendo/64/index.jsx
@@ -1,3 +1,5 @@
+import React from 'react';
+
 import picture from './assets/picture.png';
 import content from './assets/content.png';
 
--- src/emulators/other/odyssey/index.jsx
+++ src/emulators/other/odyssey/index.jsx
@@ -1,3 +1,5 @@
+import React from 'react';
+
 import picture from './assets/picture.png';
 
 export default {
