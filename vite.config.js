a63ded65d408d7beaa5218262366cb90264564ca
Lucien Le Roux <rshn.kabu@gmail.com>
2022-07-24 22:18:08 +0200

    try fix ci

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
+import { resolve } from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    root: resolve(__dirname, 'src'),
+    build: {
+        outDir: resolve(__dirname, 'dist'),
+    },
     plugins: [react()],
 });
